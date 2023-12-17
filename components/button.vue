deb0ad5a08f3867d43e298fa7fad52b2205c4400
Ali Shamas <ali.shamas111225@gmail.com>
2023-12-17 12:29:19 +0200

    Log in

---
+++ components/button.vue
@@ -0,0 +1,34 @@
+<template>
+    <button :id="buttonId" :class="buttonClasses" @click="handleClick">
+      {{ buttonText }}
+    </button>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      buttonId: {
+        type: String,
+        default: '',
+      },
+      buttonClasses: {
+        type: String,
+        default: '',
+      },
+      buttonText: {
+        type: String,
+        default: '',
+      },
+    },
+    methods: {
+      handleClick() {
+        this.$emit('click');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
---
+++ components/input.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="form-group flex flex-col gap-1">
+      <label :for="inputId">{{ label }}</label>
+      <input
+        :type="type"
+        :id="inputId"
+        :placeholder="placeholder"
+        :class="inputClasses"
+        v-model="inputValue"
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      label: {
+        type: String,
+        required: true,
+      },
+      placeholder: {
+        type: String,
+        default: '',
+      },
+      inputClasses: {
+        type: String,
+        default: '',
+      },
+      type: {
+        type: String,
+        default: 'text',
+      },
+      value: {
+        type: [String, Number],
+        default: '',
+      },
+    },
+   
+  };
+  </script>
+  
+  <style scoped>
+
+  .form-group {
+    margin-bottom: 15px;
+  }
+  </style>
+  
